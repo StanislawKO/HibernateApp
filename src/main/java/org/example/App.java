081295b5a4ca1db6e927a055a05ae36f170e9c2d
Stanislaav Kozh <printks@yandex.ru>
2024-01-18 21:14:55 +0300

    work with hibernate

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -5,6 +5,8 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.List;
+
 /**
  * Hello world!
  */
@@ -18,9 +20,25 @@ public class App {
         try {
             session.beginTransaction();
 
-            Person person = session.get(Person.class, 1);
-            System.out.println(person.getName());
-            System.out.println(person.getAge());
+//            Person person1 = new Person("Test1", 20);
+//            Person person2 = new Person("Test2", 30);
+//            Person person3 = new Person("Test3", 40);
+//
+//            session.save(person1);
+//            session.save(person2);
+//            session.save(person3);
+
+//            List<Person> people = session.createQuery("from Person where age > 30").getResultList();
+//            List<Person> people = session.createQuery("from Person where name like 'T%'").getResultList();
+//            session.createQuery("update Person set name='T%' where age < 30").executeUpdate();
+            session.createQuery("delete from Person where age < 30").executeUpdate();
+
+//            for (Person person :
+//                    people) {
+//                System.out.println(person);
+//            }
+
+
 
             session.getTransaction().commit();
 
--- src/main/java/org/example/model/Person.java
+++ src/main/java/org/example/model/Person.java
@@ -1,9 +1,6 @@
 package org.example.model;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "Person")
@@ -11,6 +8,7 @@ public class Person {
 
     @Id
     @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(name = "name")
@@ -22,8 +20,7 @@ public class Person {
     public Person() {
     }
 
-    public Person(int id, String name, int age) {
-        this.id = id;
+    public Person(String name, int age) {
         this.name = name;
         this.age = age;
     }
@@ -50,5 +47,11 @@ public class Person {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "name='" + name + '\'' +
+                ", age=" + age;
     }
 }
